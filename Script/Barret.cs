ae7ba64c347261109b2416427cc51b285816a3d4
SH_K <122723235+SeungHee-K@users.noreply.github.com>
2023-02-12 23:30:30 +0900

    드럼통(아이템) - by.정지원

--- Script/Barret.cs
+++ Script/Barret.cs
@@ -7,13 +7,12 @@ public class Barret : MonoBehaviour
     public GameObject UIImage;
     void Start()
     {
-        Destroy(gameObject,10f);
+        Destroy(gameObject,10f); // 드럼통 10초 후 삭제
     }
     
     public void Death()
     {
-        Instantiate(UIImage, this.transform.position,Quaternion.identity);
-                       
+        Instantiate(UIImage, this.transform.position,Quaternion.identity);                      
         
         Destroy(gameObject);
     }
